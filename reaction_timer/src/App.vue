98a8e81856fb714a155352a9cf99864745cbfb1a
kashishagrawal1907 <kashish.agrawal1907@gmail.com>
2021-02-11 11:05:37 +0530

    reaction_timer

---
+++ reaction_timer/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <h1>Reaction Timer</h1>
+  <button class="start" @click="start" :disabled="is_playing">Play</button>
+  <Block v-if="is_playing" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score" />
+</template>
+
+<script>
+import Block from "./components/Block.vue";
+import Results from "./components/Results.vue";
+export default {
+  name: "App",
+  components: {Results, Block },
+  data() {
+    return {
+      is_playing: false,
+      delay: null,
+      score: null,
+      showResults: false
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.is_playing = true;
+      this.showResults = false
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.is_playing = false
+      this.showResults = true
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+button{
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+</style>
---
+++ reaction_timer/src/components/Block.vue
@@ -0,0 +1,49 @@
+<template>
+  <div v-if="showBlock" @click="stopTimer" class="block">Click me !!</div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+      timer : null,
+      reactionTime: 0
+    };
+  },
+  methods:{
+    startTimer(){
+      this.timer =setInterval(()=>{
+        this.reactionTime+=5
+      }, 5)
+    },
+    stopTimer(){
+      clearInterval(this.timer)
+      console.log(this.reactionTime)
+      this.$emit('end', this.reactionTime)
+    },
+
+    },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer()
+    }, this.delay);
+  },
+
+};
+</script>
+
+<style>
+.block {
+  background: #0faf87;
+  color: white;
+  width: 25%;
+  height: 25%;
+  border-radius: 15px;
+  align-content: center;
+  padding: 5%;
+  margin: 10px auto;
+}
+</style>
